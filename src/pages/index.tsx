128983fb00914f8267584c89dda8a68466d45414
hrayr <developer.hovakimyan@gmail.com>
2023-03-21 16:19:11 +0400

    Add Tailwind to project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Welcome</h1>
+        <h1 className="text-3xl font-bold text-center">Welcome</h1>
       </main>
     </>
   )
