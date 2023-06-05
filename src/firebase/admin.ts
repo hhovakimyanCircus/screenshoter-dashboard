41f0712d443bc8a434184080b075572225193ff2
Artak <artakom98@gmail.com>
2023-06-05 12:45:02 +0400

    Implement auth0 and firebase custom token authorization

---
+++ src/firebase/admin.ts
@@ -0,0 +1,24 @@
+import admin from 'firebase-admin';
+
+try {
+  const config = {
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+  };
+
+  admin.initializeApp({
+    credential: admin.credential.cert(config),
+    databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  });
+} catch (error) {
+  /*
+   * We skip the "already exists" message which is
+   * not an actual error when we're hot-reloading.
+   */
+  if (!/already exists/u.test(error.message)) {
+    console.error('Firebase admin initialization error', error.stack);
+  }
+}
+
+export default admin;
